ca57eb08f94e2f53f9a3e9f227596542c283d5d2
Ayush Shaw <ayushshaw127@gmail.com>
2022-05-07 13:49:56 +0530

    removed blog

--- js/nav_responsive.js
+++ js/nav_responsive.js
@@ -2,7 +2,6 @@ let home = document.getElementById("home")
 let socials = document.getElementById("socials")
 let community = document.getElementById("community")
 let download = document.getElementById("download")
-let blog = document.getElementById("blog")
 let store = document.getElementById("store")
 let menu = document.getElementById("menu")
 let btn = document.getElementById("donate")
@@ -13,7 +12,6 @@ menu.addEventListener('click', ()=>{
         home.classList.remove("hidden")
         community.classList.remove("hidden")
         download.classList.remove("hidden")
-        blog.classList.remove("hidden")
         store.classList.remove("hidden")
         donate.classList.remove("hidden")
         visible=true;
@@ -21,7 +19,6 @@ menu.addEventListener('click', ()=>{
         home.classList.add("hidden")
         community.classList.add("hidden")
         download.classList.add("hidden")
-        blog.classList.add("hidden")
         store.classList.add("hidden")
         donate.classList.add("hidden")
         visible=false;
